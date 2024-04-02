39f8d04400b0800ad1e41e30d622e34991096d8f
SaiKoteshVasa <saikotesh8227@gmail.com>
2024-04-02 20:59:57 +0530

    Create search page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,49 +23,49 @@ export default function Header() {
     }
   }, [location.search]);
   return (
-    <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
+    <header className="bg-stone-400 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-stone-600">Real</span>
+            <span className="text-stone-700">Estate</span>
           </h1>
         </Link>
         <form
           onSubmit={handleSubmit}
-          className='bg-slate-100 p-3 rounded-lg flex items-center'
+          className="bg-stone-100 p-3 rounded-lg flex items-center"
         >
           <input
-            type='text'
-            placeholder='Search...'
-            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline:none w-24 sm:w-64"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button>
-            <FaSearch className='text-slate-600' />
+            <FaSearch className="text-stone-600" />
           </button>
         </form>
-        <ul className='flex gap-4'>
-          <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+        <ul className="flex gap-10">
+          <Link to="/">
+            <li className="hidden sm:inline text-stone-700 hover:underline">
               Home
             </li>
           </Link>
-          <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+          <Link to="/about">
+            <li className="hidden sm:inline text-stone-700 hover:underline">
               About
             </li>
           </Link>
-          <Link to='/profile'>
+          <Link to="/profile">
             {currentUser ? (
               <img
-                className='rounded-full h-7 w-7 object-cover'
+                className="rounded-full h-7 w-7 object-cover"
                 src={currentUser.avatar}
-                alt='profile'
+                alt="profile"
               />
             ) : (
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
+              <li className="text-stone-700 hover:underline">Sign in</li>
             )}
           </Link>
         </ul>
